1bcce04c71529e0ee24e2bf85c0b4edc707c9db5
Jack <3jbc22@gmail.com>
2019-05-08 10:17:01 -0400

    Update the location of where the sys prints the gpa

--- src/script.js
+++ src/script.js
@@ -1,6 +1,7 @@
 var loc = window.location.href
 var boolLoc = loc.includes("summitcds.myschoolapp.com")
 
+
 const GPA_MAP = {100:4.3,99:4.3,98:4.3,97:4.3,96:4.0,95:4.0,94:4.0,93:3.7,92:3.7,91:3.7,90:3.7,89:3.3,88:3.3,87:3.3,86:3.0,85:3.0,84:3.0,83:2.7,82:2.7,81:2.7,80:2.7,79:2.3,78:2.3,77:2.3,76:2.0,75:2.0,74:2.0,73:1.7,72:1.7,71:1.7,70:1.7,69:1.3,68:1.3,67:1.3,66:1.0,65:1.0,64:1.0,63:0.7,62:0.7,61:0.7,60:0.7,59:0,58:0,57:0,56:0,55:0,54:0,53:0,52:0,51:0,50:0,49:0,48:0,47:0,46:0,45:0,44:0,43:0,42:0,41:0,40:0,39:0,38:0,37:0,36:0,35:0,34:0,33:0,32:0,31:0,30:0,29:0,28:0,27:0,26:0,25:0,24:0,23:0,22:0,21:0,20:0,19:0,18:0,17:0,16:0,15:0,14:0,13:0,12:0,11:0,10:0,9:0,8:0,7:0,6:0,5:0,4:0,3:0,2:0,1:0,0:0}
 
 const gpa_avg = []
@@ -137,14 +138,19 @@ function averageIze() {
 }
 
 function appendGrades(gradesMaster) {
-	var parent = document.getElementById("courses").getElementsByClassName("col-md-12")[0]
-	var parentE = document.createElement("div")
-	var e = document.createElement("strong")
-	parentE.className = "pull-left bold h3-line-height"
-	parentE.style.marginTop = "4px"
-	parentE.style.marginLeft = "8px"
-	e.innerHTML = "GPA: "
-	e.innerHTML = e.innerHTML + gradesMaster
-	parentE.appendChild(e)
-	parent.appendChild(parentE)
+	document.getElementById("performanceCollapse").getElementsByTagName("div")[0].id = "gpaParent"
+
+	var z = document.createElement('a');
+	z.innerHTML = ' GPA';
+	z.className = 'accordion-toggle';
+	var x = document.createElement("span")
+	x.className = 'label label-success'
+	x.innerHTML = gradesMaster
+	var br = document.createElement("br")
+	var hr = document.createElement("hr")
+	var y = document.getElementById("gpaParent")
+	y.appendChild(x);
+	y.appendChild(z);
+	y.appendChild(br);
+	y.appendChild(hr);
 }
