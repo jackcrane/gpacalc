54eaa809328bb61a85d46a59ca766fa171c2d1e2
Jack <3jbc22@gmail.com>
2019-09-06 15:06:34 -0400

    fixed my absolute idiocy

--- src/script.js
+++ src/script.js
@@ -1,5 +1,5 @@
 var loc = window.location.href
-var boolLoc = loc.includes("summitcds.myschoolapp.com")
+var boolLoc = loc.includes(".myschoolapp.com")
 
 
 const GPA_MAP = {100:4.3,99:4.3,98:4.3,97:4.3,96:4.0,95:4.0,94:4.0,93:3.7,92:3.7,91:3.7,90:3.7,89:3.3,88:3.3,87:3.3,86:3.0,85:3.0,84:3.0,83:2.7,82:2.7,81:2.7,80:2.7,79:2.3,78:2.3,77:2.3,76:2.0,75:2.0,74:2.0,73:1.7,72:1.7,71:1.7,70:1.7,69:1.3,68:1.3,67:1.3,66:1.0,65:1.0,64:1.0,63:0.7,62:0.7,61:0.7,60:0.7,59:0,58:0,57:0,56:0,55:0,54:0,53:0,52:0,51:0,50:0,49:0,48:0,47:0,46:0,45:0,44:0,43:0,42:0,41:0,40:0,39:0,38:0,37:0,36:0,35:0,34:0,33:0,32:0,31:0,30:0,29:0,28:0,27:0,26:0,25:0,24:0,23:0,22:0,21:0,20:0,19:0,18:0,17:0,16:0,15:0,14:0,13:0,12:0,11:0,10:0,9:0,8:0,7:0,6:0,5:0,4:0,3:0,2:0,1:0,0:0}
@@ -68,9 +68,10 @@ function grade() {
 	if(calcGrade(elem21) == "nc") {} else {doStuffWith(elem21)}
 	if(calcGrade(elem22) == "nc") {} else {doStuffWith(elem22)}
 	if(calcGrade(elem23) == "nc") {} else {doStuffWith(elem23)}
+	
 
 	appendGrades(averageIze())
-
+	writeInsights()
 	// calcGrade(elem2)
 	// calcGrade(elem3)
 	// calcGrade(elem4)
@@ -86,9 +87,33 @@ function grade() {
 function doStuffWith(elem) {
 	gpa_avg.push(calcGrade(elem))
 	populateModalTable(elem)
+	checkForInsights(elem)
 }
 
+var insightsArray = new Array()
+var insightsArrayLength = 0
 
+function checkForInsights(elem) {
+	var insightsClassGrade = getClassGrade(elem).replace('%','').toString()
+	var icg = insightsClassGrade
+	var insightsClassName = getClassName(elem)
+	// BELOW LINE: BE SURE TO REMOVE TRUE BEFORE RELEASE
+	if(true || icg == 96 || icg == 93 || icg == 89 || icg == 86 || icg == 83 || icg == 79 || icg == 76 || icg == 73 || icg == 69 || icg == 66 || icg == 63 || icg <= 59) {
+		insightsArray.push(insightsClassName)
+		insightsArray.push(insightsClassGrade)
+		insightsArrayLength += 1
+	}
+}
+
+function writeInsights() {
+	console.log('function run')
+	for(var i = 0; i<insightsArrayLength; i++) {
+		var className = insightsArray[i]
+		var classGrade = insightsArray[i+1]
+		console.log(className)
+		console.log(classGrade)
+	}
+}
 
 function getElements() {
 	var classes = document.getElementsByClassName('bb-tile-content-section')[3]
